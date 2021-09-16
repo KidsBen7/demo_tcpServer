fb8b74f51bb3a4cc9c49944b5eaae10b0fb1b6a8
datnc3 <chanhdat.ng@gmail.com>
2021-09-16 21:09:15 +0700

    Update PORT

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 // Load the TCP Library
-net = require('net');
+net = require("net");
 
 // Keep track of the chat clients
 var clients = [];
 
 // Start a TCP Server
-net.createServer(function (socket) {
+net
+  .createServer(function (socket) {
+    // Identify this client
+    socket.name = socket.remoteAddress + ":" + socket.remotePort;
 
-  // Identify this client
-  socket.name = socket.remoteAddress + ":" + socket.remotePort
+    // Put this new client in the list
+    clients.push(socket);
 
-  // Put this new client in the list
-  clients.push(socket);
+    // Send a nice welcome message and announce
+    // socket.write("Welcome " + socket.name.toString() + "\n");
+    // broadcast(socket.name + " joined the chat\n", socket);
+    console.log(socket.name + " joined\n");
 
-  // Send a nice welcome message and announce
-  // socket.write("Welcome " + socket.name.toString() + "\n");
-  // broadcast(socket.name + " joined the chat\n", socket);
-  console.log(socket.name + " joined\n")
-
-  // Handle incoming messages from clients.
-  socket.on('data', function (data) {
-    broadcast(socket.name + " >>> " + data, socket);
-  });
-
-  // Remove the client from the list when it leaves
-  socket.on('end', function () {
-    clients.splice(clients.indexOf(socket), 1);
-    broadcast(socket.name + " disconnected.\n");
-  });
-  
-  // Send a message to all clients
-  function broadcast(message, sender) {
-    clients.forEach(function (client) {
-      // Don't want to send it to sender
-      if (client === sender) return;
-      client.write(message);
+    // Handle incoming messages from clients.
+    socket.on("data", function (data) {
+      broadcast(socket.name + " >>> " + data, socket);
     });
-    // Log it to the server output too
-    process.stdout.write(message)
-  }
 
-}).listen(7777);
+    // Remove the client from the list when it leaves
+    socket.on("end", function () {
+      clients.splice(clients.indexOf(socket), 1);
+      broadcast(socket.name + " disconnected.\n");
+    });
+
+    // Send a message to all clients
+    function broadcast(message, sender) {
+      clients.forEach(function (client) {
+        // Don't want to send it to sender
+        if (client === sender) return;
+        client.write(message);
+      });
+      // Log it to the server output too
+      process.stdout.write(message);
+    }
+  })
+  .listen(5000);
 
 // Put a friendly message on the terminal of the server.
-console.log("Chat server running at port 7777\n");
-
+console.log("Chat server running at port 5000\n");
